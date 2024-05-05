4abfddf189ffc49efbcaf90cc9a4440dc967022d
Sean Perry <sean.hyatt.perry@gmail.com>
2024-05-04 17:24:22 -0700

    feat: adds glup and foudnation to build site

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,40 @@
 const util = require('node:util');
-const exec = util.promisify(require('node:child_process').exec);
-const { watch, series } = require('gulp');
+const fs = require('fs');
+const middleware = require('./middleware');
 
-async function build(cb) {
-    const { stdout, stderr } = await exec("bundle exec jekyll build");
+const exec = util.promisify(require('node:child_process').exec);
+const { watch, src, dest , series} = require('gulp');
+var browserSync = require('browser-sync').create();
+
+
+function copy_foundation_files(cb) {
+    //move foundation css to build 
+    foundation = [
+        "./node_modules/foundation-sites/dist/css/foundation.min.css",
+        "./node_modules/foundation-sites/dist/js/foundation.min.js"
+    ]
+   
+    return src(foundation).pipe(dest('_site/assets/')) 
+}
+
+async function create_command(command) {
+    const { stdout, stderr } = await exec(command);
     console.log(`stdout: ${stdout}`);
     console.error(`stderr: ${stderr}`);
+}
 
+async function prod_build(cb) {
+    await create_command("bundle exec jekyll build --config _config.yml")
+    cb();
+}
+
+async function dev_build(cb) {
+    await create_command("bundle exec jekyll build --config _local_config.yml")
+    cb();
+}
+
+function reload(cb) {
+    browserSync.reload()
     cb();
 }
 
@@ -14,9 +42,15 @@ function livereload(cb) {
     // All events will be watched
 
     fileGlops = ["**.html", "**.md", "_layouts/**", "_includes/**", "_posts/**", "_sass/**", "assets/**"]
-    watch(fileGlops, { events: 'all', ignoreInitial: true }, async function(cb) {
-        console.log("start new build!!!")
-        await build(cb)
+    watch(fileGlops, { events: 'all', ignoreInitial: false }, series(copy_foundation_files, dev_build, reload));
+    browserSync.init({
+        server: {
+            baseDir: "./_site/"
+        },
+        // WARNING: IF YOU FIND YOURSELF EDITING THIS FILE
+        // then you may need to aslo check if the prod redict rules in .htaccess
+        // work for the case your are trying to fix (use `jekyll serve`)
+        middleware: [middleware.rewriteMiddleware, middleware.error404Middleware]
     });
     cb();
     console.log("watcher created, stop watcher with ctrl+c")
@@ -28,5 +62,5 @@ function defaultTask(cb) {
 }
 
 exports.default = defaultTask
-exports.build = build
-exports.watch = livereload
+exports.build = series(copy_foundation_files, prod_build)
+exports.watch = series(copy_foundation_files, livereload)
---
+++ middleware.js
@@ -0,0 +1,60 @@
+// thank you chatgpt for this template for me to edit.
+// I spent so much time trying to find documentation
+
+// WARNING: IF YOU FIND YOURSELF EDITING THIS FILE
+// then you may need to aslo check if the prod redict rules in .htaccess
+// work for the case your are trying to fix (use `jekyll serve`)
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+const exp = require('constants');
+
+// Define location of pages
+const dirname = "./_site"
+
+// Custom middleware to handle 404 errors
+function error404Middleware(req, res, next) {
+    const url = req.url
+    fs.stat(path.join(dirname, url), (err, stats) => {
+        if (!err) {
+            next();
+        } else {
+            console.log(url, " is missing")
+            res.writeHead(301, { 'Location': "/404.html" });
+            res.end();
+        }
+    })
+}
+
+// Custom middleware to handle trailing slashes and append .html extension
+function rewriteMiddleware(req, res, next) {
+    let  url = req.url
+    if (url[-1] == "/") {
+        url = url.splice(0, -1)
+    }
+    if (url == "/.html") {
+        res.writeHead(301, { 'Location': '/index.html' });
+        res.end();
+        
+    }
+    
+    const extension = path.extname(url);
+    if (!extension) {
+        // If the request does not have an extension add .html on it
+        fs.stat(path.join(dirname, url + '.html'), (err, stats) => {
+            if (!err) {
+                res.writeHead(301, { 'Location': url + '.html' });
+                res.end();
+            } else {
+                next();
+            }
+        });
+    }  else {
+        next();
+    }
+}
+
+module.exports = {
+    error404Middleware,
+    rewriteMiddleware
+};
