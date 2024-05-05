14237aaf704195be9807c9d97d2428fb6c3fa10b
Sean Perry <sean.hyatt.perry@gmail.com>
2024-05-04 21:18:38 -0700

    fix: fix so it serves page instead of rewrites url

--- middleware.js
+++ middleware.js
@@ -2,7 +2,7 @@
 // I spent so much time trying to find documentation
 
 // WARNING: IF YOU FIND YOURSELF EDITING THIS FILE
-// then you may need to aslo check if the prod redict rules in .htaccess
+// then you may need to also check if the prod redict rules in .htaccess
 // work for the case your are trying to fix (use `jekyll serve`)
 const http = require('http');
 const path = require('path');
@@ -19,11 +19,40 @@ function error404Middleware(req, res, next) {
         if (!err) {
             next();
         } else {
-            console.log(url, " is missing")
+            //console.log(url, " is missing")
             res.writeHead(301, { 'Location': "/404.html" });
             res.end();
         }
     })
+}
+
+// Utility function to get content type based on file extension
+function getContentType(filePath) {
+    const extname = path.extname(filePath);
+    switch (extname) {
+        case '.html':
+            return 'text/html';
+        case '.css':
+            return 'text/css';
+        case '.js':
+            return 'text/javascript';
+        default:
+            return 'text/plain';
+    }
+}
+
+function serveContent(filePath, res) {
+    fs.readFile(filePath, (err, data) => {
+        if (!err) {
+            // Set appropriate Content-Type based on file extension
+            const contentType = getContentType(filePath);
+            res.writeHead(200, { 'Content-Type': contentType });
+            res.write(data);
+            res.end();
+        } else {
+            next();
+        }
+    });
 }
 
 // Custom middleware to handle trailing slashes and append .html extension
@@ -33,8 +62,9 @@ function rewriteMiddleware(req, res, next) {
         url = url.splice(0, -1)
     }
     if (url == "/.html") {
-        res.writeHead(301, { 'Location': '/index.html' });
-        res.end();
+        serveContent(path.join(dirname, "/index.html"), res)
+        // res.writeHead(301, { 'Location': '/index.html' });
+        // res.end();
         
     }
     
@@ -43,8 +73,9 @@ function rewriteMiddleware(req, res, next) {
         // If the request does not have an extension add .html on it
         fs.stat(path.join(dirname, url + '.html'), (err, stats) => {
             if (!err) {
-                res.writeHead(301, { 'Location': url + '.html' });
-                res.end();
+                serveContent(path.join(dirname, url + '.html'), res)
+                // res.writeHead(301, { 'Location': url + '.html' });
+                // res.end();
             } else {
                 next();
             }
