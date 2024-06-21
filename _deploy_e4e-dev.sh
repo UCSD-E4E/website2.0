0d1d12b038172a1ce0192238f074944c5cc1b835
Nathan Hui <nthui@ucsd.edu>
2024-06-21 14:13:37 -0700

    ci: Allowing permanent cache

--- _deploy_e4e-dev.sh
+++ _deploy_e4e-dev.sh
@@ -1,11 +1,12 @@
 PATH="$PATH:/home/deploy/.nvm/versions/node/v22.3.0/bin:/home/deploy/.rbenv/shims"
 echo $PATH
-BRANCH="main"
 HOME_DIR=`pwd`
-CLONE_DIR=`mktemp -d`
+CLONE_DIR=`/home/deploy/workspace/e4e_tools/website2.0`
 JEKYLL_ENV="production"
-git clone --depth 1 --single-branch --branch=$BRANCH https://github.com/UCSD-E4E/website2.0.git $CLONE_DIR
+# git clone --depth 1 --single-branch --branch=$BRANCH https://github.com/UCSD-E4E/website2.0.git $CLONE_DIR
 cd $CLONE_DIR
+git fetch --all
+git reset --hard origin/mmain
 mkdir -p $CLONE_DIR/cache
 mkdir -p $CLONE_DIR/cache/resize
 npm ci
@@ -13,4 +14,3 @@ npx gulp build -j '--config _config.yml,_e4e_dev_config.yml'
 date >> _site/last_deployed.txt
 rsync -r -e "ssh -i $HOME/.ssh/id_e4edev" --progress $CLONE_DIR/_site/ e4e-dev@e4e-dev.ucsd.edu:htdocs/
 cd $HOME_DIR
-rm -rf $CLONE_DIR
