6da24991cfc5aa64dd78e4c152b54b5f73637f43
Nathan Hui <nthui@ucsd.edu>
2024-06-21 14:58:44 -0700

    ci: Fixed directory

--- _deploy_e4e-dev.sh
+++ _deploy_e4e-dev.sh
@@ -2,7 +2,7 @@ set -e -x
 PATH="$PATH:/home/deploy/.nvm/versions/node/v22.3.0/bin:/home/deploy/.rbenv/shims"
 echo $PATH
 HOME_DIR=`pwd`
-CLONE_DIR=`/home/deploy/workspace/e4e_tools/website2.0`
+CLONE_DIR="/home/deploy/workspace/e4e_tools/website2.0"
 JEKYLL_ENV="production"
 # git clone --depth 1 --single-branch --branch=$BRANCH https://github.com/UCSD-E4E/website2.0.git $CLONE_DIR
 cd $CLONE_DIR
